0c1933ddd75b1cfff896980c610545c51d556e62
위성원 <tkrhkaos12@naver.com>
2023-04-15 18:26:54 +0900

    feat: add next.js 13

---
+++ apps/web/app/head.tsx
@@ -0,0 +1,9 @@
+const Head = () => {
+  return (
+    <>
+      <title>My Next.js App</title>
+    </>
+  );
+};
+
+export default Head;
---
+++ apps/web/app/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <h1>Hello World! From NextJs 13</h1>;
+};
+
+export default Page;
